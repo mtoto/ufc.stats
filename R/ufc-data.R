523e7ec16b4896cd1096d7c636245b101fad65e1
mtoto <tszilagyi@outlook.com>
2019-10-22 22:19:19 +0200

    first draft

---
+++ R/ufc-data.R
@@ -0,0 +1,19 @@
+#' UFC Fight Statistics
+#'
+#' Data UFC Fights from the very beginning to today.
+#'
+#' @docType data
+#'
+#' @usage data(ufc_stats)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references
+#' (\href{http://www.ufcstats.com/statistics/events/completed}{UFC Stats})
+#'
+#'
+#' @examples
+#' data(ufc_stats)
+"ufc_stats"
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ufc.stats)
+
+test_check("ufc.stats")
