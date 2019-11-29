be673d242a46311a02c6c55f23b232b5b5990aba
mtoto <tszilagyi@outlook.com>
2019-11-29 17:25:18 +0100

    new data tests, new gh action for data testing before sync

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -14,3 +14,45 @@ test_that("No missing values (except event level cols)", {
     names(ufc_stats)[colSums(is.na(ufc_stats)) > 0],
     c("Date","Location","Attendance"))
 })
+
+test_that("There's a winner and a loser for every non-draw, non-nc fight", {
+  expect_equal(
+    ufc_stats %>%
+      group_by(Id) %>%
+      filter(any(Winner=='W')) %>%
+      ungroup() %>%
+      count(),
+    ufc_stats %>%
+      group_by(Id) %>%
+      filter(any(Winner=='L')) %>%
+      ungroup() %>%
+      count())
+})
+
+test_that("Draws have the same 'Winner' value for both fighters", {
+  expect_equal(
+    ufc_stats %>%
+      group_by(Id) %>%
+      filter(any(Winner=='D')) %>%
+      ungroup() %>%
+      count(),
+    ufc_stats %>%
+      group_by(Id) %>%
+      filter(Winner=='D') %>%
+      ungroup() %>%
+      count())
+})
+
+test_that("No-Contest fights have the same 'Winner' value for both fighters", {
+  expect_equal(
+    ufc_stats %>%
+      group_by(Id) %>%
+      filter(any(Winner=='NC')) %>%
+      ungroup() %>%
+      count(),
+    ufc_stats %>%
+      group_by(Id) %>%
+      filter(Winner=='NC') %>%
+      ungroup() %>%
+      count())
+})
