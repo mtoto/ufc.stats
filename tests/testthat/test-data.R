b37495ed17fd320dd59ec447490189a597e459d3
mtoto <tszilagyi@outlook.com>
2019-12-29 22:46:45 +0100

    fail action on test error

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,5 +1,6 @@
 context("data regression tests")
 options(tibble.print_min = 20)
+
 # load ufc data
 data("ufc_stats")
 
@@ -12,47 +13,47 @@ test_that("Duplicate rows", {
 test_that("No missing values (except event level cols)", {
   expect_equal(
     names(ufc_stats)[colSums(is.na(ufc_stats)) > 0],
-    c("Date","Location","Attendance"))
+    c("fight_date","location","attendance"))
 })
 
 test_that("There's a winner and a loser for every non-draw, non-nc fight", {
   expect_equal(
     ufc_stats %>%
-      group_by(Id) %>%
-      filter(any(Winner=='W')) %>%
+      group_by(id) %>%
+      filter(any(winner=='W')) %>%
       ungroup() %>%
       count(),
     ufc_stats %>%
-      group_by(Id) %>%
-      filter(any(Winner=='L')) %>%
+      group_by(id) %>%
+      filter(any(winner=='L')) %>%
       ungroup() %>%
       count())
 })
 
-test_that("Draws have the same 'Winner' value for both fighters", {
+test_that("Draws have the same 'winner' value for both fighters", {
   expect_equal(
     ufc_stats %>%
-      group_by(Id) %>%
-      filter(any(Winner=='D')) %>%
+      group_by(id) %>%
+      filter(any(winner=='D')) %>%
       ungroup() %>%
       count(),
     ufc_stats %>%
-      group_by(Id) %>%
-      filter(Winner=='D') %>%
+      group_by(id) %>%
+      filter(winner=='D') %>%
       ungroup() %>%
       count())
 })
 
-test_that("No-Contest fights have the same 'Winner' value for both fighters", {
+test_that("No-Contest fights have the same 'winner' value for both fighters", {
   expect_equal(
     ufc_stats %>%
-      group_by(Id) %>%
-      filter(any(Winner=='NC')) %>%
+      group_by(id) %>%
+      filter(any(winner=='NC')) %>%
       ungroup() %>%
       count(),
     ufc_stats %>%
-      group_by(Id) %>%
-      filter(Winner=='NC') %>%
+      group_by(id) %>%
+      filter(winner=='NC') %>%
       ungroup() %>%
       count())
 })
