a26b9b943b0529016c488d0004578577af5b56a5
mtoto <tszilagyi@outlook.com>
2020-01-01 17:54:42 +0100

    documentation, action fix

--- R/refresh_data.R
+++ R/refresh_data.R
@@ -3,8 +3,6 @@
 #' Retrieve the latest ufc_stats data.frame from GitHub. This function will also automatically overwrite the current rda contained in the package.
 #' @export
 #' @return The latest version of ufc_stats data.frame.
-#' @examples
-#' refresh_data()
 
 refresh_data <- function() {
   ufc_data <- url("https://raw.github.com/mtoto/ufc.stats/master/data/ufc_stats.rda")
--- R/ufc-data.R
+++ R/ufc-data.R
@@ -6,12 +6,10 @@
 #'
 #' @usage data(ufc_stats)
 #'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
-#'
 #' @keywords datasets
 #'
 #' @references
-#' (\href{http://www.ufcstats.com/statistics/events/completed}{UFC Stats})
+#' http://www.ufcstats.com/statistics/events/completed
 #'
 #'
 #' @examples
