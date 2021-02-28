36ba644010539941fca71351474c034ced11dea0
mtoto <tszilagyi@outlook.com>
2021-02-28 20:44:18 +0100

    update package website

--- R/refresh_data.R
+++ R/refresh_data.R
@@ -1,12 +1,12 @@
 #' Refresh UFC data with the latest fights
 #' @description
 #' This function updates the rda file contained in the package folder data/ufc_stats.rda.
-#' @export
-#' @return The latest version of ufc_stats data.frame.
 #' @examples
 #' refresh_data()
 #' # now what we load into memory is the latest dataset
 #' data("ufc_stats)
+#' @export
+
 
 refresh_data <- function() {
   load(url("https://s3-us-west-1.amazonaws.com/ufc.stats/final/ufc_stats.rda"))
